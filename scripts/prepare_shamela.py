344b08869302e48b0c777ef0ad1bfc5d94ddbc3e
HUiLAA <imp.hui1.ma@proton.me>
2026-08-24 07:51:25 +0300

    fix some issues with tools path

--- scripts/prepare_shamela.py
+++ scripts/prepare_shamela.py
@@ -5,19 +5,17 @@ import urllib3
 
 DATASET_DUMP_URL = "https://dev.shamela.ws/downloads/shamela-database-1448.zip"
 ARCHIVE_DIST = (
-	Path(__file__).resolve().parent / ".." / "datasets_archive" / "shamela-database-1448.zip"
+	Path(__file__).resolve().parent
+	/ ".."
+	/ "datasets_archive"
+	/ "shamela-database-1448.zip"
 )
 
 shamela_extractor_relative = "tools/dataset-prep/shamela-extractor"
-shamela_extractor = Path(
-	os.path.join(__file__, "..", shamela_extractor_relative)
-).resolve()
+shamela_extractor = Path(__file__).resolve().parent / ".." / shamela_extractor_relative
 
 dataset_preparer_relative = "tools/dataset-prep/dataset-preparer"
-shamela_extractor = Path(
-	os.path.join(__file__, "..", dataset_preparer_relative)
-).resolve()
-
+dataset_preparer = Path(__file__).resolve().parent / ".." / dataset_preparer_relative
 
 http = urllib3.PoolManager()
 
@@ -27,15 +25,18 @@ def main() -> None:
 
 
 def download_archive() -> None:
-    ARCHIVE_DIST.parent.mkdir(parents=True, exist_ok=True)
-    
-    with http.request("GET", DATASET_DUMP_URL, preload_content=False) as response:
-	    with open(ARCHIVE_DIST, "wb") as f:
-	        while True:
-	            chunk = response.read(1024 * 1024)  # 1 MB
-	            if not chunk:
-	                break
-	            f.write(chunk)
+	"""
+	Download the archive as chucks
+	"""
+	ARCHIVE_DIST.parent.mkdir(parents=True, exist_ok=True)
+
+	with http.request("GET", DATASET_DUMP_URL, preload_content=False) as response:
+		with open(ARCHIVE_DIST, "wb") as f:
+			while True:
+				chunk = response.read(1024 * 1024)	# 1 MB
+				if not chunk:
+					break
+				f.write(chunk)
 
 
 def unpack_archive() -> None:
@@ -59,4 +60,4 @@ def resolve_path(path) -> Path:
 
 
 if __name__ == "__main__":
-	download_archive()
+	continue
